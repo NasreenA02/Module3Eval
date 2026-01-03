98d1ed5e9230acd840d08d6f4d1e096548faf05a
NasreenA02 <nassu324@gmail.com>
2026-01-03 20:14:13 +0530

    3 pages of app

--- restaurent-app/src/pages/Login.jsx
+++ restaurent-app/src/pages/Login.jsx
@@ -1,1 +1,32 @@
-import { useContex, useRef, useEffect, useState } from "react";
+import { useContext, useRef, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+export default function Login() {
+    const emailRef=useRef();
+    const {login} =
+    useContext(AuthContext);
+    const navigate = useNavigate();
+    const [email, setEmail] = useState(" ");
+    const [password, setPassword]= useState(" ");
+    useEffect(() => {
+        emailRef.current.focus();
+    }, []
+);
+const handleLogin=()=>{
+    const role=login(email, password);
+    if(!role) {
+        alert("wrong email or password");
+    }else {
+        alert("Login succesfull");
+        navigate(role==="admin" ? "/admin" : "/customer");
+    }
+    };
+    return (
+        <div>
+            <h2>Login</h2>
+            <input ref={emailRef} placeholder="Email" onChange={e=>setEmail(e.target.value)}/>
+            <input type="password" placeholder="Password" onChange={e=>setPassword(e.target.value)}/>
+            <button onClick={handleLogin}>Login</button>
+        </div>
+    );
+}
